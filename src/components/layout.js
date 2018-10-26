0876cb4026f1c40877d38d5a1072cd64a0a6e799
Eric Rosenberg <roseneric15@gmail.com>
2018-10-26 09:40:26 -0400

    Remove unnecessary import

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
 import 'normalize.css';
-import Theme from './Theme';
 import Header from './header';
 import './layout.css';
 
